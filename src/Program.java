408148196451ea02c29e18dc3c31479072cca3f8
Tran Minh Nhat <tranminhnhat3006@gmail.com>
2018-09-21 19:10:29 +0700

    gfgdf

--- src/Program.java
+++ src/Program.java
@@ -4,5 +4,7 @@ public class Program {
     public static void main(String[] args) {
         GameWindow gameWindow = new GameWindow();
         gameWindow.mainLoop();
+
+        // Thang Ha pha vkl !!!
     }
 }
